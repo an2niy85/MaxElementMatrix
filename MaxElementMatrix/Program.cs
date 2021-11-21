fa918fb9062bd4ce1128c6a9dca2419e651cf838
Антон Красноперов <an2niy85@mail.ru>
2021-11-21 13:34:04 +0400

    Добавьте файлы проекта.

---
+++ MaxElementMatrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxElementMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 10;
+            int[,] matrix = new int[size, size];
+            Random rand = new Random();
+            int max = 0, k = 0, m = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = rand.Next(9);
+                }
+            }
+
+            foreach (int item in matrix)
+            {
+                Console.Write(item + " ");
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i,j]>max)
+                    {
+                        max = matrix[i, j];
+                        k = i;
+                        m = j;
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+            matrix[k, m] = 0;
+
+            foreach (int item in matrix)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
