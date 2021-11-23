f1830d37cd0442f6b309e62cebb5739ca7b7b094
Антон Красноперов <an2niy85@mail.ru>
2021-11-23 18:15:10 +0400

    Исправил теперь ищет все макс элементы

--- MaxElementMatrix/Program.cs
+++ MaxElementMatrix/Program.cs
@@ -12,52 +12,52 @@ namespace MaxElementMatrix
         {
             const int size = 10;
             int[,] matrix = new int[size, size];
-            Random rand = new Random();
-            int max = 0, k = 0, m = 0;
+            Random random = new Random();
+            int max = 0;
+        
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
-                    matrix[i, j] = rand.Next(10);
+                    matrix[i, j] = random.Next(10);
                 }
             }
 
             Console.WriteLine("Элементы исходной матрицы:");
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     Console.Write(matrix[i,j] + " ");
                 }
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if (matrix[i,j]>max)
                     {
-                        max = matrix[i, j];
-                        k = i;
-                        m = j;
+                        max = matrix[i, j];                        
                     }
                 }
             }
-            Console.WriteLine("\nНаибольший элемент матрицы с индексами: " + k + " " + m);
-            Console.WriteLine(max);
-            matrix[k, m] = 0;
+            Console.WriteLine($"\nНаибольший элемент матрицы: {max}");
 
             Console.WriteLine("\nЭлементы полученной матрицы:");
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
+                    if (matrix[i,j] == max)
+                    {
+                        matrix[i, j] = 0;
+                    }
                     Console.Write(matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-
             Console.ReadLine();
         }
     }
